b74dd56b2573b094748473b4fa653470f64e7b3b
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-02-28 08:56:21 +0300

    feat: add styles to WelcomePage component

--- client/src/pages/WelcomePage.tsx
+++ client/src/pages/WelcomePage.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import {Box, Typography} from "@mui/material";
+
+const headerLength = '80px'
 
 const WelcomePage = () => {
+
+
     return (
-        <>
-            Welcome
-        </>
+        <Box
+            sx={{
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+            }}
+            style={{
+                minHeight: `calc(100vh - ${headerLength})`,
+            }}
+        >
+            <Typography sx={{
+
+                color: '#010101',
+                fontWeight: '400',
+                fontSize: '85px',
+                fontFamily: 'inherit',
+                lineHeight:' 126px',
+                letterSpacing: '0.05em',
+                textTransform:' uppercase',
+            }}
+            >
+                Welcome!
+            </Typography>
+        </Box>
     );
 };
 
