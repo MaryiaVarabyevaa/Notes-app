38a36800bc9ba9d1efdce4c1e8b3d27e7758e524
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-03-02 10:51:40 +0300

    feat: add scroll to new note

--- client/src/pages/NotesPage/NotesList.tsx
+++ client/src/pages/NotesPage/NotesList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { Box, IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { addNote, getNotes, updateQueueNumber } from '../../http/noteAPI';
@@ -12,7 +12,9 @@ import Note from './Note';
 const NotesList = () => {
   const [notes, setNotes] = useState<INote[]>([]);
   const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [isAdded, setIsAdded] = useState<boolean>(false);
   const [currentNote, setCurrentNote] = useState<INote | null>(null);
+  const lastNote = useRef<HTMLInputElement>(null);
 
   const getAllNotes = async (): Promise<INote[]> => {
     const notes = await getNotes();
@@ -30,6 +32,7 @@ const NotesList = () => {
     });
     const newNotes = [ ...notes, newNote ];
     setNotes(newNotes);
+    setIsAdded(!isAdded);
   };
 
   useEffect(() => {
@@ -38,13 +41,16 @@ const NotesList = () => {
     });
   },[]);
 
+  useEffect(() => {
+    lastNote?.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }, [isAdded]);
+
   const dragStartHandle = (e: React.DragEvent<HTMLDivElement>, note: INote) => {
     setCurrentNote(note);
   };
 
   const dragOverHandler = (e:React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
-
   };
 
   const dropHandler = async (e:React.DragEvent<HTMLDivElement>, note: INote): Promise<void> => {
@@ -66,22 +72,21 @@ const NotesList = () => {
 
   return (
     <Box sx={{ display: 'flex', gap: '30px' }}>
-      <Box sx={{ width: '1140px', display: 'flex', gap: '30px' }}>
+      <Box sx={{ width: '1140px', display: 'flex', gap: '30px', overflowX: 'auto', whiteSpace: 'nowrap' }}>
         {
-          notes.length !== 0 && notes.sort(sortFunc).map((note) => (<Box
+          notes.length !== 0 && notes.sort(sortFunc).map((note, index) => (<Box
             onDragStart={(e) => dragStartHandle(e, note)}
             onDragOver={(e) => dragOverHandler(e)}
             onDrop={(e) => dropHandler(e, note)}
             draggable={true}
+            ref={index === notes.length - 1? lastNote : null}
             key={note.id} sx={{
               bgcolor: `${note.color}`,
               padding: '8px 15px 0 14px',
               ...flex('column', 'space-between'),
-              // ...size('360px', '600px'),
-              // flex: 'flex: 0 0 360px',
-              // overflow: 'hidden', whiteSpace: 'nowrap',
-              // cursor: 'grab',
-              // height: '600px',
+              flex: '0 0 360px',
+              cursor: 'grab',
+              height: '600px',
             }}>
             <Note { ...note } />
           </Box>))
